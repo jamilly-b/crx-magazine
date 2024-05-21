9a202fbbfc7353af747385d40eedc38a76ff380f
jamilly-b <jbm2@discente.ifpe.edu.br>
2024-05-21 17:32:27 -0300

    first commit

---
+++ Magazine/src/main/java/com/crx/Magazine/CrxMagazineApplication.java
@@ -0,0 +1,13 @@
+package com.crx.Magazine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrxMagazineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrxMagazineApplication.class, args);
+	}
+
+}
---
+++ Magazine/src/test/java/com/crx/Magazine/CrxMagazineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crx.Magazine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrxMagazineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
