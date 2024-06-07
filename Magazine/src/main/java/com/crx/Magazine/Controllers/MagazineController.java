b4ff2a58d471c98edb6d5eaba246601f36f45aa2
jamilly-b <jbm2@discente.ifpe.edu.br>
2024-06-07 17:36:31 -0300

    pagina do escritor dinâmica

--- Magazine/src/main/java/com/crx/Magazine/Controllers/MagazineController.java
+++ Magazine/src/main/java/com/crx/Magazine/Controllers/MagazineController.java
@@ -1,7 +1,9 @@
 package com.crx.Magazine.Controllers;
 
 import com.crx.Magazine.Models.Entities.Artigo;
+import com.crx.Magazine.Models.Entities.User;
 import com.crx.Magazine.Models.Repository.CrxMagazineRepository;
+import com.crx.Magazine.Models.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,5 +41,22 @@ public class MagazineController {
         return mv;
     }
 
+    @Autowired
+    UserRepository userRepository;
+    @RequestMapping(value = "/autor/{nome}/{id}", method = RequestMethod.GET)
+    public ModelAndView getAutorDetails(@PathVariable("nome") String nome, @PathVariable("id") Integer id) {
+        ModelAndView mv = new ModelAndView("autor");
+        Optional<User> autor = userRepository.findById(id);
+
+        if (autor.isPresent() && autor.get().getNome().equals(nome)) {
+            List<Artigo> artigos = magazineRepository.findByEscritorId(id);
+            mv.addObject("autor", autor.get());
+            mv.addObject("artigos", artigos);
+        } else {
+            mv.addObject("errorMessage", "Autor não encontrado");
+            mv.setViewName("error");
+        }
+        return mv;
+    }
 
 }
--- Magazine/src/main/java/com/crx/Magazine/Models/Repository/CrxMagazineRepository.java
+++ Magazine/src/main/java/com/crx/Magazine/Models/Repository/CrxMagazineRepository.java
@@ -2,9 +2,15 @@ package com.crx.Magazine.Models.Repository;
 
 import com.crx.Magazine.Models.Entities.Artigo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CrxMagazineRepository extends JpaRepository<Artigo, Integer> {
     List<Artigo> findByTitulo(String titulo);
+    List<Artigo> findByEscritorId(Integer escritorId);
+    @Query("SELECT a FROM Artigo a JOIN FETCH a.escritor WHERE a.id = :id")
+    Optional<Artigo> findByIdWithEscritor(@Param("id") Integer id);
 }
